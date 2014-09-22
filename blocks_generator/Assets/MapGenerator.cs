73556c0d4345cd678da4c0cd7f7f9bca4483ef50
brunoguerra <brunoguerrait@gmail.com>
2014-09-22 03:32:32 -0300

    first commit

---
+++ blocks_generator/Assets/MapGenerator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+	public GameObject blockPref;
+
+	// Use this for initialization
+	void Start () {
+		GameObject block;
+
+
+		for(int x=-100; x<100; x++) {
+			for(int z=-100; z<100; z++) {
+				block = (GameObject)Instantiate(blockPref);
+				block.transform.localPosition = new Vector3(x*block.transform.localScale.x, 0, z*block.transform.localScale.x);
+			}
+		}
+
+
+		for(int x=-90; x<90; x+=10) {
+			for(int z=-90; z<90; z+=15) {
+				for (int y=5; y>0; y--) {
+					block = Instantiate(blockPref, new Vector3(x*blockPref.transform.localScale.x, y, z*blockPref.transform.localScale.x), Quaternion.identity) as GameObject;
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ blocks_generator/Assets/ModifyMesh.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ModifyMesh : MonoBehaviour {
+
+
+	void Start() {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.Clear ();
+
+		mesh.Clear();
+		mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 0, 0)};
+
+		float uvS = 0.33f;
+
+		mesh.uv = new Vector2[] {new Vector2(uvS, 0), new Vector2(0, uvS), new Vector2(uvS, 0.uvS), new Vector2(uvS, 0)};
+		mesh.triangles = new int[] {0, 1, 2, 
+			                        0, 2, 3/*, 2, 1, 0*/};
+		
+	}
+	
+	void Update() {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		int i = 0;
+		while (i < vertices.Length) {
+			vertices[i] += Vector3.up * Time.deltaTime;
+			i++;
+		}
+		mesh.vertices = vertices;
+		mesh.RecalculateBounds();
+	}
+}
